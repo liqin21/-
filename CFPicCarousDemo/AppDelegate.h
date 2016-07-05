04ce454a285a5c1af5ead543d45cfdf29c5e97f7
chenfeijie <chenfeijie@chenfeijiedemac-mini.local>
2016-07-05 15:51:37 +0800

    Initial Commit

---
+++ CFPicCarousDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CFPicCarousDemo
+//
+//  Created by chenfeijie on 16/7/5.
+//  Copyright © 2016年 chenfeijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
